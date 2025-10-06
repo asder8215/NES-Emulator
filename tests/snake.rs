1d95faba1a5bbabaa82f863465ddfdd8519b2b99
Mahdi Ali-Raihan <asder8215@gmail.com>
2025-10-05 23:19:53 -0400

    6502 snake game works, you can run cargo test --tests snake

---
+++ tests/snake.rs
@@ -0,0 +1,197 @@
+use nes_emulator::cpu::CPU;
+use nes_emulator::cpu::mem::Mem;
+use rand::Rng;
+use sdl2::EventPump;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+
+/// Records the WASD, arrow key, or escape input and writes that into
+/// 0xFF
+fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
+    for event in event_pump.poll_iter() {
+        match event {
+            // if we press escape, end game
+            Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => std::process::exit(0),
+            // Move up
+            Event::KeyDown {
+                keycode: Some(Keycode::W),
+                ..
+            }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Up),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x77);
+            }
+            // Move down
+            Event::KeyDown {
+                keycode: Some(Keycode::S),
+                ..
+            }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Down),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x73);
+            }
+            // Move left
+            Event::KeyDown {
+                keycode: Some(Keycode::A),
+                ..
+            }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Left),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x61);
+            }
+            // Move right
+            Event::KeyDown {
+                keycode: Some(Keycode::D),
+                ..
+            }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Right),
+                ..
+            } => {
+                cpu.mem_write(0xff, 0x64);
+            }
+            _ => { /* do nothing */ }
+        }
+    }
+}
+
+
+/// Interprets the color of a pixel (represented as 1 byte)
+/// in memory
+fn color(byte: u8) -> Color {
+    // keep in mind that our snake game uses 1 byte for color
+    // on each pixel while sdl2 expects 3 bytes
+    // so we take the 1 byte from the 0x200..0x600 region and map
+    // it to a 3 byte color value
+    match byte {
+        0 => sdl2::pixels::Color::BLACK,
+        1 => sdl2::pixels::Color::WHITE,
+        2 | 9 => sdl2::pixels::Color::GREY,
+        3 | 10 => sdl2::pixels::Color::RED,
+        4 | 11 => sdl2::pixels::Color::GREEN,
+        5 | 12 => sdl2::pixels::Color::BLUE,
+        6 | 13 => sdl2::pixels::Color::MAGENTA,
+        7 | 14 => sdl2::pixels::Color::YELLOW,
+        _ => sdl2::pixels::Color::CYAN,
+    }
+}
+
+/// Reads the whole screen state of the game from 0x200..0x600 and copies the color
+/// of each byte over to the frame buffer of the sdl2 window
+fn read_screen_state(cpu: &CPU, frame: &mut [u8; 32 * 3 * 32]) -> bool {
+    let mut frame_idx = 0;
+    let mut update = false;
+    for i in 0x0200..0x600 {
+        let color_idx = cpu.mem_read(i as u16);
+        let (b1, b2, b3) = color(color_idx).rgb();
+        if frame[frame_idx] != b1 || frame[frame_idx + 1] != b2 || frame[frame_idx + 2] != b3 {
+            frame[frame_idx] = b1;
+            frame[frame_idx + 1] = b2;
+            frame[frame_idx + 2] = b3;
+            update = true;
+        }
+        frame_idx += 3;
+    }
+    update
+}
+
+#[test]
+fn snake_test() {
+    // initialize sdl2
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window(
+            "6502 Snake Game",    // name of the window
+            (32.0 * 10.0) as u32, // width in pixels
+            (32.0 * 10.0) as u32, // heigh in pixel
+        )
+        .position_centered() // center window
+        .build() // finish build
+        .unwrap();
+
+    // Initialize the Canvas window
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    // Initialize Event Pump to start collecting inputs
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    // drawing target will be scaled by 10
+    canvas.set_scale(10.0, 10.0).unwrap();
+
+    let creator = canvas.texture_creator();
+    // Texture has size of 32 by 32 and we're using the RGB24 color palette
+    let mut texture = creator
+        .create_texture_target(PixelFormatEnum::RGB24, 32, 32)
+        .unwrap();
+
+    // 6502 Snake Game Code: https://gist.github.com/wkjagt/9043907
+    // 0xFE ~ RNG
+    // 0xFF ~ A code of the last pressed button (W, A, S, D, arrow key, escape)
+    // [0x0200..0x0600] ~ This whole region is the screen of the game starting
+    //                    from the top left corner (we have a 32 by 32 matrix)
+    //                    where each element contains some u8 value corresponding
+    //                    to a color
+    // [0x0600..]       ~ This region forward is where the game_code data below
+    //                    resides in
+    let game_code = vec![
+        0x20, 0x06, 0x06, 0x20, 0x38, 0x06, 0x20, 0x0d, 0x06, 0x20, 0x2a, 0x06, 0x60, 0xa9, 0x02,
+        0x85, 0x02, 0xa9, 0x04, 0x85, 0x03, 0xa9, 0x11, 0x85, 0x10, 0xa9, 0x10, 0x85, 0x12, 0xa9,
+        0x0f, 0x85, 0x14, 0xa9, 0x04, 0x85, 0x11, 0x85, 0x13, 0x85, 0x15, 0x60, 0xa5, 0xfe, 0x85,
+        0x00, 0xa5, 0xfe, 0x29, 0x03, 0x18, 0x69, 0x02, 0x85, 0x01, 0x60, 0x20, 0x4d, 0x06, 0x20,
+        0x8d, 0x06, 0x20, 0xc3, 0x06, 0x20, 0x19, 0x07, 0x20, 0x20, 0x07, 0x20, 0x2d, 0x07, 0x4c,
+        0x38, 0x06, 0xa5, 0xff, 0xc9, 0x77, 0xf0, 0x0d, 0xc9, 0x64, 0xf0, 0x14, 0xc9, 0x73, 0xf0,
+        0x1b, 0xc9, 0x61, 0xf0, 0x22, 0x60, 0xa9, 0x04, 0x24, 0x02, 0xd0, 0x26, 0xa9, 0x01, 0x85,
+        0x02, 0x60, 0xa9, 0x08, 0x24, 0x02, 0xd0, 0x1b, 0xa9, 0x02, 0x85, 0x02, 0x60, 0xa9, 0x01,
+        0x24, 0x02, 0xd0, 0x10, 0xa9, 0x04, 0x85, 0x02, 0x60, 0xa9, 0x02, 0x24, 0x02, 0xd0, 0x05,
+        0xa9, 0x08, 0x85, 0x02, 0x60, 0x60, 0x20, 0x94, 0x06, 0x20, 0xa8, 0x06, 0x60, 0xa5, 0x00,
+        0xc5, 0x10, 0xd0, 0x0d, 0xa5, 0x01, 0xc5, 0x11, 0xd0, 0x07, 0xe6, 0x03, 0xe6, 0x03, 0x20,
+        0x2a, 0x06, 0x60, 0xa2, 0x02, 0xb5, 0x10, 0xc5, 0x10, 0xd0, 0x06, 0xb5, 0x11, 0xc5, 0x11,
+        0xf0, 0x09, 0xe8, 0xe8, 0xe4, 0x03, 0xf0, 0x06, 0x4c, 0xaa, 0x06, 0x4c, 0x35, 0x07, 0x60,
+        0xa6, 0x03, 0xca, 0x8a, 0xb5, 0x10, 0x95, 0x12, 0xca, 0x10, 0xf9, 0xa5, 0x02, 0x4a, 0xb0,
+        0x09, 0x4a, 0xb0, 0x19, 0x4a, 0xb0, 0x1f, 0x4a, 0xb0, 0x2f, 0xa5, 0x10, 0x38, 0xe9, 0x20,
+        0x85, 0x10, 0x90, 0x01, 0x60, 0xc6, 0x11, 0xa9, 0x01, 0xc5, 0x11, 0xf0, 0x28, 0x60, 0xe6,
+        0x10, 0xa9, 0x1f, 0x24, 0x10, 0xf0, 0x1f, 0x60, 0xa5, 0x10, 0x18, 0x69, 0x20, 0x85, 0x10,
+        0xb0, 0x01, 0x60, 0xe6, 0x11, 0xa9, 0x06, 0xc5, 0x11, 0xf0, 0x0c, 0x60, 0xc6, 0x10, 0xa5,
+        0x10, 0x29, 0x1f, 0xc9, 0x1f, 0xf0, 0x01, 0x60, 0x4c, 0x35, 0x07, 0xa0, 0x00, 0xa5, 0xfe,
+        0x91, 0x00, 0x60, 0xa6, 0x03, 0xa9, 0x00, 0x81, 0x10, 0xa2, 0x00, 0xa9, 0x01, 0x81, 0x10,
+        0x60, 0xa2, 0x00, 0xea, 0xea, 0xca, 0xd0, 0xfb, 0x60,
+    ];
+
+    let mut screen_state = [0_u8; 32 * 3 * 32];
+    let mut rng = rand::thread_rng();
+
+    // Instantiate the CPU and loads the snake game into memory
+    let mut cpu = CPU::new();
+    cpu.test_load(&game_code);
+    cpu.reset();
+
+    // Run the game
+    cpu.test_run_with_callback(move |cpu| {
+        // grab user input
+        handle_user_input(cpu, &mut event_pump);
+        // write randomly generated number for the color of apple
+        cpu.mem_write(0xFE, rng.gen_range(1, 16));
+
+        // read the screen state from the cpu and renders it accordingly
+        // on the canvas
+        if read_screen_state(cpu, &mut screen_state) {
+            texture.update(None, &screen_state, 32 * 3).unwrap();
+            canvas.copy(&texture, None, None).unwrap();
+            canvas.present();
+        }
+
+        // introduce a 70000 ns delay so that the game doesn't run too fastc
+        ::std::thread::sleep(std::time::Duration::new(0, 70_000));
+    });
+}
